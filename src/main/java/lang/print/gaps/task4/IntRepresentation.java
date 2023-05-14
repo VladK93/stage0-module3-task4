6af6491a64cb134694cc56b788eb19b0871a4b85
Vlad Kulish <vladkulish19@gmail.com>
2023-05-14 23:08:04 +0300

    done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    int x = 99;
+    int y = 104;
+    int s= 97;
+    int d =114;
+        System.out.println((char) x+" "+(char)y+" "+(char)s+" "+(char)d);
     }
 }
